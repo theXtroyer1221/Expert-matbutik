925abdc2d5164067c0ccd7033161d16728d9b0e6
theXtroyer1221 <theXtroyer1221@users.noreply.github.com>
2020-01-11 23:39:32 +0100

    done with slideshow also

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config(TEMPLATES_AUTO_RELOAD=True)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 @app.route("/")
 def index():
-    product = ["apple", "banana", "cherry"]
+    product = ["apple", "banana", "product1", "product2", "orange"]
     return render_template("index.html",
                            len=len(product),
                            product_image=product)
