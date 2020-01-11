f7eae1606e2e4cc108f50491c6d2ab322f665617
theXtroyer1221 <theXtroyer1221@users.noreply.github.com>
2020-01-11 22:33:53 +0100

    fixed the slideshow 100 and fully done and server side imrpovments

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+app.config(TEMPLATES_AUTO_RELOAD=True)
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    product = ["apple", "banana", "cherry"]
+    return render_template("index.html",
+                           len=len(product),
+                           product_image=product)
 
 
 if __name__ == "__main__":
