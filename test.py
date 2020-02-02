7cb34b5d9d358b354e38cb0bb28dd36f738568cb
theXtroyer1221 <theXtroyer1221@users.noreply.github.com>
2020-02-02 22:22:01 +0100

    finsih varor need just to change the text also social

--- test.py
+++ test.py
@@ -3,11 +3,6 @@ import json
 with open("json/product.json", "r", encoding='utf8') as f:
     product_list = json.load(f)
 
-print(product_list["Chark"][0]["namn"])
 for product in product_list:
-    print(product)
-print("-----")
-#for i in range(len(product_list)):
-#    for j in product_list[i]:
-#        print(j["namn"])
-print(product_list)
+    for i in product_list[product]:
+        print(i)
