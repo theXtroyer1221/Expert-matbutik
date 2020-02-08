20d7bfa7f4cacbf0e4bc9a0431df3a3370f26944
theXtroyer1221 <theXtroyer1221@users.noreply.github.com>
2020-02-08 17:29:31 +0100

    emailing the contact is working

--- main.py
+++ main.py
@@ -4,12 +4,38 @@ from forms import SignUpForm
 import json
 import random
 
+import smtplib
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "ExpertMatbutik"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 with open("json/week-rea.json", "r", encoding='utf8') as f:
     week_rea = json.load(f)
+
+
+def send_mail(name, mail, message):
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+
+        server.login("jaddou2005@gmail.com", "wccjxrgoffwxmulu")
+    except Exception as e:
+        print(e)
+
+    subject = "Ett medelande fron kund(kontakta oss)"
+    body = f"Kundens namn: '{name}'\n epost: '{mail}'\n\n meddelande: {message}"
+
+    msg = f"Subject: {subject}\n\n{body}"
+
+    server.sendmail("jaddou2005@gmail.com",
+                    "jadeddin.alnabki@elev.norrkoping.se", msg)
+
+    server.quit()
+
+    print("* Email sent")
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -21,9 +47,7 @@ def index():
         mail = result.get("mail")
         message = result["message"]
 
-        print(name)
-        print(mail)
-        print(message)
+        send_mail(name, mail, message)
 
     product = ["store", "sattelite", "unicorn", "paket"]
 
